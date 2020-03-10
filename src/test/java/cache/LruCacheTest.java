3b0ea94ca66dc873c360fd6682df05d371ea257b
JuniorJainsahab <p.jain3322@gmail.com>
2020-03-10 23:27:50 +0530

    adding test for to check we are accesing the correct value.

--- src/test/java/cache/LruCacheTest.java
+++ src/test/java/cache/LruCacheTest.java
@@ -2,13 +2,12 @@ package cache;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class LruCacheTest {
 
     @Test
-    public void shouldRemoveTheOldestValueWhenCacheSizeIsExceeded() {
+    public void shouldCheckElementIsPresentOrNotWhenCacheSizeIsExceeded() {
         LruCache<Integer, Integer> lruCache = new LruCache<>(4);
         lruCache.put(1, 1);
         lruCache.put(2, 3);
@@ -40,4 +39,17 @@ public class LruCacheTest {
         assertTrue(lruCache.get(6).isPresent());
     }
 
+    @Test
+    public void shouldCheckWeAccessedTheCorrectValue() {
+        LruCache<Integer, Integer> lruCache = new LruCache<>(4);
+        lruCache.put(1, 1);
+        lruCache.put(2, 3);
+        lruCache.put(3, 4);
+        lruCache.put(4, 6);
+
+        assertEquals(new Integer(1),lruCache.get(1).get());
+        assertEquals(new Integer(3),lruCache.get(2).get());
+        assertEquals(new Integer(4),lruCache.get(3).get());
+        assertEquals(new Integer(6),lruCache.get(4).get());
+    }
 }

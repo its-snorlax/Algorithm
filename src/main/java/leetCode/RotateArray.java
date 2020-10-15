7349c1afa90ef3cb9b62afe427fc6508be11f37a
JuniorJainsahab <p.jain3322@gmail.com>
2020-10-15 23:40:14 +0530

    implementing improve version of right roation and left rotation.

--- src/main/java/leetCode/RotateArray.java
+++ src/main/java/leetCode/RotateArray.java
@@ -1,19 +1,33 @@
+//https://leetcode.com/problems/rotate-array/ (Right Rotation)
+//https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem (Left Rotation)
+
 package leetCode;
 
 public class RotateArray {
-    public int[] rotate(int[] nums, int k) {
-        if (nums.length == 1) return nums;
-        int[] temp = new int[nums.length];
+    public int[] rotateRight(int[] nums, int k) {
         k = k % nums.length;
-        for (int i=0; i<nums.length; i++) {
-            int positionAfterRotation = k + i;
-            if (positionAfterRotation < nums.length){
-                temp[positionAfterRotation] = nums[i];
-            } else {
-                temp[positionAfterRotation - nums.length] = nums[i];
-            }
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+        return nums;
+    }
+
+    public int[] rotateLeft(int[] nums, int k) {
+        k = k % nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, nums.length - k - 1);
+        reverse(nums, nums.length - k, nums.length - 1);
+        return nums;
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
         }
-        return temp;
     }
 }
 
--- src/test/java/leetCode/RotateArrayTest.java
+++ src/test/java/leetCode/RotateArrayTest.java
@@ -10,12 +10,26 @@ public class RotateArrayTest {
     @Test
     public void sample1() {
         int[] input = {1, 2, 3, 4, 5, 6, 7};
-        assertEquals("[5, 6, 7, 1, 2, 3, 4]", Arrays.toString(new RotateArray().rotate(input, 3)));
+        assertEquals("[5, 6, 7, 1, 2, 3, 4]", Arrays.toString(new RotateArray().rotateRight(input, 3)));
     }
 
     @Test
     public void sample2() {
         int[] input = {-1, -100, 3, 99};
-        assertEquals("[3, 99, -1, -100]", Arrays.toString(new RotateArray().rotate(input, 2)));
+        assertEquals("[3, 99, -1, -100]", Arrays.toString(new RotateArray().rotateRight(input, 2)));
     }
+
+    @Test
+    public void sample3() {
+        int[] input = {1, 2, 3, 4, 5};
+        assertEquals("[5, 1, 2, 3, 4]", Arrays.toString(new RotateArray().rotateLeft(input, 4)));
+    }
+
+    @Test
+    public void sample4() {
+        int[] input = {41, 73, 89, 7, 10, 1, 59, 58, 84, 77, 77, 97, 58, 1, 86, 58, 26, 10, 86, 51};
+        assertEquals("[77, 97, 58, 1, 86, 58, 26, 10, 86, 51, 41, 73, 89, 7, 10, 1, 59, 58, 84, 77]",
+                Arrays.toString(new RotateArray().rotateLeft(input, 10)));
+    }
+
 }

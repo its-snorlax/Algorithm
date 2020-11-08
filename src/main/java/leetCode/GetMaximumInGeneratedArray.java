f3ab55a4363e2c617de76d6c3c9afd4687962735
JuniorJainsahab <p.jain3322@gmail.com>
2020-11-08 09:46:42 +0530

    solving question number 1.

--- src/main/java/leetCode/GetMaximumInGeneratedArray.java
+++ src/main/java/leetCode/GetMaximumInGeneratedArray.java
@@ -1,5 +1,25 @@
 package leetCode;
 
-public class GetMaximuminGeneratedArray {
-    
+import java.util.ArrayList;
+
+public class GetMaximumInGeneratedArray {
+    public int getMaximumGenerated(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return 1;
+        int max = 1;
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(0);
+        list.add(1);
+        int i = 1;
+        while (list.size() < n + 1) {
+            list.add(2 * i, list.get(i));
+            if (list.size() < n + 1) {
+                int newNum = list.get(i) + list.get(i + 1);
+                list.add(2 * i + 1, newNum);
+                max = Math.max(max, newNum);
+            }
+            i++;
+        }
+        return max;
+    }
 }

56af70979811f431099f93c925e06a7a0831a632
JuniorJainsahab <p.jain3322@gmail.com>
2020-11-15 21:13:18 +0530

    speeding up for 938. Range Sum of BST

--- src/main/java/leetCode/RangeSumOfBST.java
+++ src/main/java/leetCode/RangeSumOfBST.java
@@ -4,13 +4,11 @@ package leetCode;
 import tree.TreeNode;
 
 public class RangeSumOfBST {
-    int sum = 0;
 
     public int rangeSumBST(TreeNode root, int low, int high) {
         if (root == null) return 0;
-        if (root.val >= low && root.val <= high) sum += root.val;
-        rangeSumBST(root.left, low, high);
-        rangeSumBST(root.right, low, high);
-        return sum;
+        if (root.val < low) return rangeSumBST(root.right, low, high);
+        else if (root.val > high) return rangeSumBST(root.left, low, high);
+        return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
     }
 }

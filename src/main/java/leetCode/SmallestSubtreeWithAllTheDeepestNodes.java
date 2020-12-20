d378afc89aa62dce538ddb8dc96ae2c28f30d9be
Juniorjainsahab <p.jain3322@gmail.com>
2020-12-20 21:05:20 +0530

    removing pair class with AbstractMap

--- src/main/java/leetCode/SmallestSubtreeWithAllTheDeepestNodes.java
+++ src/main/java/leetCode/SmallestSubtreeWithAllTheDeepestNodes.java
@@ -1,24 +1,26 @@
 //https://leetcode.com/problems/smallest-subtree-with-all-the-deepest-nodes/
 package leetCode;
 
-import javafx.util.Pair;
 import tree.TreeNode;
+
+import java.util.AbstractMap;
+import java.util.Map;
 
 public class SmallestSubtreeWithAllTheDeepestNodes {
     public TreeNode subtreeWithAllDeepest(TreeNode root) {
         return helper(root).getValue();
     }
 
-    private Pair<Integer, TreeNode> helper(TreeNode root) {
-        if (root == null) return new Pair<>(0, null);
-        Pair<Integer, TreeNode> left = helper(root.left);
-        Pair<Integer, TreeNode> right = helper(root.right);
+    private Map.Entry<Integer, TreeNode> helper(TreeNode root) {
+        if (root == null) return new AbstractMap.SimpleEntry<>(0, null);
+        Map.Entry<Integer, TreeNode> left = helper(root.left);
+        Map.Entry<Integer, TreeNode> right = helper(root.right);
 
         Integer leftDepth = left.getKey();
         Integer rightDepth = right.getKey();
 
-        if (leftDepth.equals(rightDepth)) return new Pair<>(1 + leftDepth, root);
-        else if (leftDepth > rightDepth) return new Pair<>(1 + leftDepth, left.getValue());
-        else return new Pair<>(1 + rightDepth, right.getValue());
+        if (leftDepth.equals(rightDepth)) return new AbstractMap.SimpleEntry<>(1 + leftDepth, root);
+        else if (leftDepth > rightDepth) return new AbstractMap.SimpleEntry<>(1 + leftDepth, left.getValue());
+        else return new AbstractMap.SimpleEntry<>(1 + rightDepth, right.getValue());
     }
 }

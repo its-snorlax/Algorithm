d71b231e03174a22da006c901e31c0a7dd959cb7
Prayas <p.jain3322@gmail.com>
2021-01-26 19:47:04 +0530

    correcting code mistake for leetcode problem 1015

--- src/main/java/leetCode/SmallestIntegerDivisibleByK.java
+++ src/main/java/leetCode/SmallestIntegerDivisibleByK.java
@@ -3,9 +3,9 @@ package leetCode;
 
 public class SmallestIntegerDivisibleByK {
     public int smallestRepunitDivByK(int k) {
-        int remainder = 1;
+        int remainder = 0;
         for (int i = 1; i <= k; i++) {
-            remainder = (remainder * 10) + 1 % k;
+            remainder = ((remainder * 10) + 1) % k;
             if (remainder == 0) return i;
         }
         return -1;
